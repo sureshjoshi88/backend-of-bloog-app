24d0f10f14735cbb7930c33ff1c40a1f9424b1a2
sureshjoshi88 <sj3795789@gmail.com>
2025-09-25 09:59:14 +0530

    add new trycatch

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,12 @@ const router = express.Router()
 const blogschema = require('../models/blog')
 
 router.get('/blog',async(req,res)=>{
-    const blog = await blogschema.find()
-    res.status(200).json({status:true,message:"success",blog})
+    try {
+        const blog = await blogschema.find()
+        res.status(200).json({status:true,message:"success",blog})
+    } catch (error) {
+        res.status(500).json({status:false,message:"something went wrong",error:error})
+    }
 })
 
 router.post('/blog',(req,res)=>{
