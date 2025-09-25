b96329608c0131c8b2f1d46b401c3252f82d828b
sureshjoshi88 <sj3795789@gmail.com>
2025-09-25 10:20:34 +0530

    updated files and add new package

--- models/blog.js
+++ models/blog.js
@@ -1,9 +1,9 @@
  const mongoose = require('mongoose')
  const blogschema = new mongoose.Schema({
-    title:{type:String,required:true},
-    description:{type:String,required:true},
+    title:{type:String,required:true,trim:true},
+    description:{type:String,required:false},
     images:{type:String,required:true},
-    date:{type:Date,default:Date.now}
+    date:{type:Date,default:Date.now,required:true}
  },{timestamps:true})
 
  module.exports = mongoose.model("blog",blogschema)
