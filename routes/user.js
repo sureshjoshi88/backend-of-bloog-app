77c174b86a2df9f4bb238c34fe39230dfb9938b7
sureshjoshi88 <sj3795789@gmail.com>
2025-09-30 11:18:26 +0530

    update

--- routes/user.js
+++ routes/user.js
@@ -25,7 +25,7 @@ router.get('/blog', async (req, res) => {
          return res.status(200).json({ status: true, message: "success", blog:blogs })
         }
         const blog = await blogschema.find()
-        res.status(200).json({ status: true, message: "success", blog})
+        res.status(200).json({ status: true, message: "success", blog:blog})
     } catch (error) {
         res.status(500).json({ status: false, message: "something went wrong", error: error })
     }
