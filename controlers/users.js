81b094591d81c7a87f9d890426e3e6af76df65bd
sureshjoshi88 <sj3795789@gmail.com>
2025-10-01 10:57:45 +0530

    add new controller

--- controlers/users.js
+++ controlers/users.js
@@ -1,4 +1,22 @@
-const getuser = (req,res)=>{
-
+const blogschema = require("../models/blog")
+const getuser = async(req,res)=>{
+  try {
+        const {title} =req.query
+        if(title){
+          const blogs = await blogschema.find( { title: { $regex: title, $options: "i" }} 
+)
+           if (blogs.length === 0) {
+                return res.status(404).json({ 
+                    status: false, 
+                    message: "No blog found with this title" 
+                });
+            }
+         return res.status(200).json({ status: true, message: "success", blog:blogs })
+        }
+        const blog = await blogschema.find()
+        res.status(200).json({ status: true, message: "success", blog:blog})
+    } catch (error) {
+        res.status(500).json({ status: false, message: "something went wrong", error: error })
+    }
 }
 module.exports = {getuser}
--- routes/user.js
+++ routes/user.js
@@ -4,32 +4,14 @@ const blogschema = require('../models/blog')
 const multer = require("multer")
 const { body, validationResult } = require('express-validator')
 const { cloudinary } = require('../config/cloudinary')
+const { getuser } = require('../controlers/users')
 
 
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-router.get('/blog', async (req, res) => {
-    try {
-        const {title} =req.query
-        if(title){
-          const blogs = await blogschema.find( { title: { $regex: title, $options: "i" }} 
-)
-           if (blogs.length === 0) {
-                return res.status(404).json({ 
-                    status: false, 
-                    message: "No blog found with this title" 
-                });
-            }
-         return res.status(200).json({ status: true, message: "success", blog:blogs })
-        }
-        const blog = await blogschema.find()
-        res.status(200).json({ status: true, message: "success", blog:blog})
-    } catch (error) {
-        res.status(500).json({ status: false, message: "something went wrong", error: error })
-    }
-})
+router.get('/blog', getuser)
 
 
 
