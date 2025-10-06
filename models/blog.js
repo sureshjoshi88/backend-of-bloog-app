1c249f26bb0265ba3a34f9e7011483a92834ddc1
sureshjoshi88 <sj3795789@gmail.com>
2025-10-06 08:43:25 +0530

    change for blogschema

--- models/blog.js
+++ models/blog.js
@@ -1,7 +1,7 @@
  const mongoose = require('mongoose')
  const blogschema = new mongoose.Schema({
     title:{type:String,required:true,trim:true},
-    description:{type:String,required:false},
+    description:{type:String,required:true},
     image:{type:String,required:true},
     date:{type:Date,default:Date.now,required:true}
  },{timestamps:true})
