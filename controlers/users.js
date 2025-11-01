e2cf3fcb25532b7c58436cb57004673518a25bb1
sureshjoshi88 <sj3795789@gmail.com>
2025-11-01 19:53:40 +0530

    update

--- controlers/users.js
+++ controlers/users.js
@@ -72,13 +72,13 @@ const addUser = (req, res) => {
 }
 
 const updateUser = async (req,res) => {
-const {title,description,image} = req.body
+const {title,description} = req.body
 const {id} = req.params
 
  if (!mongoose.Types.ObjectId.isValid(id)) {
       return res.status(400).json({ status: false, message: "Invalid blog ID" });
     }
-if(!title || !description||image){
+if(!title || !description){
  return res.status(400).json({status:false,message:"all field are required"})
 }
 
