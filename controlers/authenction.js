6d640ff5415a62e6346cdc919222ad8a1afcdc3c
sureshjoshi88 <sj3795789@gmail.com>
2025-11-04 14:40:36 +0530

    update for sigin api

--- controlers/authenction.js
+++ controlers/authenction.js
@@ -32,7 +32,7 @@ const signupUser = async(req,res)=>{
     });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({status:false, message: 'Server error' ,error:err.message});
   }
 }
 
