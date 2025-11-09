7791c55e25418005fe8ac0daade3d5648b38fd4f
sureshjoshi88 <sj3795789@gmail.com>
2025-11-09 16:32:28 +0530

    change for db.js file

--- config/db.js
+++ config/db.js
@@ -4,9 +4,8 @@ require('dotenv').config()
 
 const connectDb = async() => {
     try {
-         await  mongoose.connect(process.env.DB_URL).then(()=>{
+         await  mongoose.connect(process.env.DB_URL)
         console.log('db connected');
-    })
     } catch (error) {
         console.log('db error ',error);
         
