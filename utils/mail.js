e82254b7ba786651d6b0db822f1e19aab4a9b994
sureshjoshi88 <sj3795789@gmail.com>
2025-11-23 18:33:23 +0530

    feat: add nodemailer transport configuration

--- utils/mail.js
+++ utils/mail.js
@@ -1,1 +1,11 @@
 const nodemailer = require("nodemailer")
+
+const transpoter = nodemailer.createTransport({
+    service:"gmail",
+    auth:{
+        user:"abc@gmail.com",
+        pass:"hduewhiu"
+    }
+    
+})
+
