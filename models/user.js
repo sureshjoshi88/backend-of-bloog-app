9c8725ae6295cd7a8837784cf8e2101922f05fdb
sureshjoshi88 <sj3795789@gmail.com>
2025-12-17 15:54:43 +0530

    add trim for user schema

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const userschema = new mongoose.Schema({
     name: {
-        type: String, required: true,
+        type: String, required: true, trim: true ,
     },
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     password: {
