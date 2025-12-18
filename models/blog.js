f726311b183fa209c7d188acc33cf22808959a34
sureshjoshi88 <sj3795789@gmail.com>
2025-12-18 20:15:23 +0530

    chnages for blog schema

--- models/blog.js
+++ models/blog.js
@@ -1,9 +1,8 @@
- const mongoose = require('mongoose')
- const blogschema = new mongoose.Schema({
-    title:{type:String,required:true,trim:true},
-    description:{type:String,required:true},
-    image:{type:String,required:true},
-    date:{type:String}
- },{timestamps:true})
+const mongoose = require('mongoose')
+const blogschema = new mongoose.Schema({
+   title: { type: String, required: true, trim: true },
+   description: { type: String, required: true },
+   image: { type: String, required: true },
+}, { timestamps: true })
 
- module.exports = mongoose.model("blog",blogschema)
+module.exports = mongoose.model("blog", blogschema)
