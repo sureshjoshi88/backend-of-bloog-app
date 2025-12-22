c36966674eb59373f7d17452c6b43d410a4fec4d
sureshjoshi88 <sj3795789@gmail.com>
2025-12-22 12:54:20 +0530

    add commnent

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express =  require('express')
+const express = require('express')
 require('dotenv').config()
 const app = express();
 const router = require('./routes/user')
-const cors  = require('cors')
+const cors = require('cors')
 const bodyParser = require('body-parser');
 const authRoute = require('./routes/authRoutes');
 const connectDb = require('./config/db');
@@ -11,12 +11,13 @@ const connectDb = require('./config/db');
 app.use(express.json())
 app.use(cors());
 app.use(bodyParser.json());
+const port = process.env.PORT
 
-connectDb().then(()=>app.listen(port,()=>{
+// connect database
+connectDb().then(() => app.listen(port, () => {
     console.log(`the server is running http://localhost:${port}`);
 })
 )
 
-app.use('/api/blogs',router)
-app.use("/api/auth",authRoute)
-const port = process.env.PORT
+app.use('/api/blogs', router)
+app.use("/api/auth", authRoute)
